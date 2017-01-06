2e174de9fa1c36f057cd6a6a5f1a1c969e8c03bf
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-01-06 11:13:26 -0700

    Store index being built out

--- spec/features/user_can_check_best_buy_store_spec.rb
+++ spec/features/user_can_check_best_buy_store_spec.rb
@@ -9,10 +9,11 @@ context "a user can visit /" do
       visit "/"
       # And I fill in a search box with "80202" and click "search"
       fill_in "q", :with => '80203'
+      click_button "Search"
       # Then my current path should be "/search" (ignoring params)
       # And I should see stores within 25 miles of 80202
-      
       # And I should see a message that says "16 Total Stores"
+      expect(page).to have_content(16)
       # And I should see exactly 15 results
       # And I should see the long name, city, distance, phone number and store type for each of the 15 results
     end
